70ba81fd6191b03ae3d2447e4454ca95d3af440a
ABTech-07 <abandekar@clarku.edu>
2025-10-28 00:30:23 -0400

    initial commit

---
+++ builf/CMakeFiles/StudentDBMS.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/akshatagbandekar/c++/ProjectsC++/Student DBMS/Student-Database-management-system/src/Admin.cpp" "CMakeFiles/StudentDBMS.dir/src/Admin.cpp.o" "gcc" "CMakeFiles/StudentDBMS.dir/src/Admin.cpp.o.d"
+  "/Users/akshatagbandekar/c++/ProjectsC++/Student DBMS/Student-Database-management-system/src/DatabaseManager.cpp" "CMakeFiles/StudentDBMS.dir/src/DatabaseManager.cpp.o" "gcc" "CMakeFiles/StudentDBMS.dir/src/DatabaseManager.cpp.o.d"
+  "/Users/akshatagbandekar/c++/ProjectsC++/Student DBMS/Student-Database-management-system/src/Student.cpp" "CMakeFiles/StudentDBMS.dir/src/Student.cpp.o" "gcc" "CMakeFiles/StudentDBMS.dir/src/Student.cpp.o.d"
+  "/Users/akshatagbandekar/c++/ProjectsC++/Student DBMS/Student-Database-management-system/src/main.cpp" "CMakeFiles/StudentDBMS.dir/src/main.cpp.o" "gcc" "CMakeFiles/StudentDBMS.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ builf/CMakeFiles/StudentDBMS.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StudentDBMS.dir/src/Admin.cpp.o"
+  "CMakeFiles/StudentDBMS.dir/src/Admin.cpp.o.d"
+  "CMakeFiles/StudentDBMS.dir/src/DatabaseManager.cpp.o"
+  "CMakeFiles/StudentDBMS.dir/src/DatabaseManager.cpp.o.d"
+  "CMakeFiles/StudentDBMS.dir/src/Student.cpp.o"
+  "CMakeFiles/StudentDBMS.dir/src/Student.cpp.o.d"
+  "CMakeFiles/StudentDBMS.dir/src/main.cpp.o"
+  "CMakeFiles/StudentDBMS.dir/src/main.cpp.o.d"
+  "StudentDBMS"
+  "StudentDBMS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StudentDBMS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
